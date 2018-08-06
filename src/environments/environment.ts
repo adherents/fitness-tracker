727c7c982f05ad40ca93557a70a93a046e2dc43b
Dima Simonov <keldor80@gmail.com>
2018-08-06 16:51:43 +0300

    git reverse

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,14 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
-import { firebase } from '../firebase';
-
 export const environment = {
   production: false,
-  firebase
+  firebase: {
+    apiKey: "AIzaSyANTqc0AIZSMxBIwfxNQLl6utR6VyFw9FQ",
+    authDomain: "fitness-tracker-angular5.firebaseapp.com",
+    databaseURL: "https://fitness-tracker-angular5.firebaseio.com",
+    projectId: "fitness-tracker-angular5",
+    storageBucket: "fitness-tracker-angular5.appspot.com",
+    messagingSenderId: "1041604391314"
+  }
 };
